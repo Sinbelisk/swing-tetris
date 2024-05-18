c8910567dda1e98c70d0b1f1fbde8514e5097bb2
Sinbelisk <rafjimray21y@gmail.com>
2024-05-18 13:52:38 +0200

    refactorizacion

--- src/tetris/gui/Game.java
+++ src/tetris/gui/Game.java
@@ -1,20 +1,59 @@
 package tetris.gui;
 
-import javax.swing.*;
+import tetris.Updatable;
+import tetris.gui.events.KeyHandler;
 
-public class Game extends JPanel{
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel implements Runnable, Updatable {
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
-
+    private Thread gameLoop;
+    private GameManager gameManager = new GameManager();
+    public Game(){
+        this.addKeyListener(new KeyHandler());
+        setFocusable(true);
+        requestFocus();
+    }
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        Board board = new Board();
-        frame.addKeyListener(board);
+        frame.setFocusable(false);
+        Game game = new Game();
         frame.setSize(WIDTH, HEIGHT);
-        frame.add(board);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.setFocusable(false);
         frame.setVisible(true);
-        board.startGameLoop();
+        game.startGameLoop();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameLoop != null) {
+            try {
+                Thread.sleep(1000/60);
+                update();
+                repaint();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void startGameLoop(){
+        gameLoop = new Thread(this);
+        gameLoop.start();
+    }
+    @Override
+    public void update() {
+        gameManager.update();
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        gameManager.draw(g2d);
     }
 }
