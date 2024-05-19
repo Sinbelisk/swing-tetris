dd141eb4d041d63bf3d1fb6911d795c249531c50
Sinbelisk <rafjimray21y@gmail.com>
2024-05-19 12:32:22 +0200

    hola

--- src/tetris/gui/panel/GamePanel.java
+++ src/tetris/gui/panel/GamePanel.java
@@ -1,9 +1,11 @@
 package tetris.gui.panel;
 
+import tetris.gui.BoardDrawer;
 import tetris.gui.Game;
 import tetris.gui.MainMenu;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
     public GamePanel() {
@@ -11,9 +13,9 @@ public class GamePanel extends JPanel {
     }
 
     private void init() {
-
+        // layout border layout
+        setLayout(new BorderLayout());
     }
-
     public static void main(String[] args) {
         JFrame frame = new JFrame();
 
@@ -24,9 +26,12 @@ public class GamePanel extends JPanel {
         frame.setSize(MainMenu.MAIN_MENU_WIDTH, MainMenu.MAIN_MENU_LENGTH);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        gamePanel.add(game);
+        gamePanel.add(game, BorderLayout.CENTER);
         frame.add(gamePanel);
         frame.setFocusable(false);
         frame.setVisible(true);
+
+        // Este metodo inicia el game loop, y me acabo  de dar cuenta que tengo que implementar uno para pausarlo o detenerlo xxdxdxdxdxdxdxdxdxdx
+        game.startGameLoop();
     }
 }
