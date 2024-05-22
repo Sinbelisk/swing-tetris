32d88010f4bcaadd281a3b3126254ff24f2979b4
lPhiNix <pablomargamer05@gmail.com>
2024-05-22 12:41:43 +0200

    arreglada cagada monumental

--- src/tetris/db/Connection.java
+++ src/tetris/db/Connection.java
@@ -1,16 +1,14 @@
 package tetris.db;
 
-import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connection {
-    InputStream inputStream = getClass().getResourceAsStream("/tetris/db/tetrisDB.db");
     public java.sql.Connection connect() {
         java.sql.Connection conn = null;
         try {
             // Especifica la ruta completa al archivo de la base de datos
-            String url = "jdbc:sqlite:"+inputStream;
+            String url = "jdbc:sqlite:src/tetris/db/tetrisDB.db";
             // Crea una conexión a la base de datos
             conn = DriverManager.getConnection(url);
             System.out.println("Connection is OK!");
